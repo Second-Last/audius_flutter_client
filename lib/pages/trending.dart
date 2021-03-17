fe37e202e3892c697d155f1e9268df044172b1f6
Gavin Zhao <74938940+GZGavinZhao@users.noreply.github.com>
2021-03-17 18:40:26 -0400

    Attempted replacing TextButton with GestureDetector

--- lib/pages/trending.dart
+++ lib/pages/trending.dart
@@ -8,65 +8,144 @@ class Trending extends StatelessWidget {
       initialIndex: 1,
       length: 3,
       child: Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          centerTitle: false,
-          title: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 0),
-            child: Text(
-              'Trending',
-              style: TextStyle(color: audiusColor),
-            ),
-          ),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(15),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(7),
-                // TODO: switch to GestureDetector to remove the overlay color
-                child: ElevatedButton( 
-                  child: Text('All Genres'),
-                  onPressed: () => print('All genres!'),
+        // extendBodyBehindAppBar: true,
+        // appBar: AppBar(
+        //   centerTitle: false,
+        //   title: Padding(
+        //     padding: const EdgeInsets.symmetric(horizontal: 0),
+        //     child: Text(
+        //       'Trending',
+        //       style: TextStyle(color: audiusColor),
+        //     ),
+        //   ),
+        //   actions: [
+        //     Padding(
+        //       padding: const EdgeInsets.all(15),
+        //       child: ClipRRect(
+        //         borderRadius: BorderRadius.circular(7),
+        //         // TODO: switch to GestureDetector to remove the overlay color
+        //         child: ElevatedButton(
+        //           child: Text('All Genres'),
+        //           onPressed: () => print('All genres!'),
+        //         ),
+        //       ),
+        //     ),
+        //   ],
+        //   bottom: TabBar(
+        //     tabs: [
+        //       Tab(
+        //         child: Column(
+        //           children: [
+        //             Icon(Icons.today),
+        //             Text('This Week'),
+        //           ],
+        //         ),
+        //       ),
+        //       Tab(
+        //         child: Column(
+        //           children: [
+        //             Icon(Icons.date_range),
+        //             Text('This Month'),
+        //           ],
+        //         ),
+        //       ),
+        //       Tab(
+        //         child: Column(
+        //           children: [
+        //             Icon(Icons.all_inclusive),
+        //             Text('This Year'),
+        //           ],
+        //         ),
+        //       ),
+        //     ],
+        //     labelPadding: EdgeInsets.zero,
+        //   ),
+        // ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 0),
+                      child: Center(
+                        child: Text(
+                          'Trending',
+                          style: TextStyle(color: audiusColor, fontSize: 22),
+                        ),
+                      ),
+                    ),
+                    Spacer(),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(7),
+                      // TODO: tweak GestureDetector
+                      child: GestureDetector(
+                        child: Container(
+                          padding: EdgeInsets.fromLTRB(15, 5, 15, 5),
+                          color: audiusColor,
+                          child: Text(
+                            'All Genres',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        onTap: () => print('All genres!'),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                child: Column(
+              Divider(
+                height: 20,
+                // thickness: 3,
+                color: audiusLightGrey,
+              ),
+              TabBar(
+                tabs: [
+                  Tab(
+                    child: Column(
+                      children: [
+                        Icon(Icons.today),
+                        Text('This Week'),
+                      ],
+                    ),
+                  ),
+                  Tab(
+                    child: Column(
+                      children: [
+                        Icon(Icons.date_range),
+                        Text('This Month'),
+                      ],
+                    ),
+                  ),
+                  Tab(
+                    child: Column(
+                      children: [
+                        Icon(Icons.all_inclusive),
+                        Text('This Year'),
+                      ],
+                    ),
+                  ),
+                ],
+                labelPadding: EdgeInsets.zero,
+              ),
+              Divider(
+                height: 0,
+                color: audiusLightGrey,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height,
+                child: TabBarView(
                   children: [
-                    Icon(Icons.today),
                     Text('This Week'),
-                  ],
-                ),
-              ),
-              Tab(
-                child: Column(
-                  children: [
-                    Icon(Icons.date_range),
                     Text('This Month'),
-                  ],
-                ),
-              ),
-              Tab(
-                child: Column(
-                  children: [
-                    Icon(Icons.all_inclusive),
                     Text('This Year'),
                   ],
                 ),
               ),
             ],
-            labelPadding: EdgeInsets.zero,
           ),
-        ),
-        body: TabBarView(
-          children: [
-            Center(child: Text('This Week')),
-            Center(child: Text('This Month')),
-            Center(child: Text('This Year')),
-          ],
         ),
       ),
     );
