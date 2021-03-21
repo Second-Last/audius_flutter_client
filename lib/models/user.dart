caec5d198e71729ce6452588f2ad39bbb4b3e830
Gavin Zhao <74938940+GZGavinZhao@users.noreply.github.com>
2021-03-21 11:52:13 -0400

    Updated User model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,9 +1,6 @@
 // import 'package:flutter/foundation.dart';
-import 'package:json_annotation/json_annotation.dart';
+// import 'package:json_annotation/json_annotation.dart';
 
-// part 'user.g.dart';
-
-@JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
 
 // Confirm those initialized values
 class User {
@@ -23,53 +20,67 @@ class User {
     required this.repostCount,
     required this.trackCount,
   });
-  
-  // TODO: Might need @JsonKey(required: true) for each... confirm
+
+  User.fromJson(Map<String, dynamic> json)
+    : albumCount = json['album_count'],
+      bio = json['bio'],
+      coverPhoto = json['coverPhoto'],
+      followeeCount = json['followee_count'],
+      followerCount = json['follower_count'],
+      handle = json['handle'],
+      id = json['id'],
+      isVerified = json['is_verified'],
+      location = json['location'],
+      name = json['name'],
+      playlistCount = json['playlist_count'],
+      profilePicture = json['profile_picture'],
+      repostCount = json['repost_count'],
+      trackCount = json['track_count'];
+
+  // TODO: do I need final here?
+  // @JsonKey(required: true)
   int albumCount = 0;
-  @JsonKey(defaultValue: false)
+  // @JsonKey(defaultValue: false)
   String? bio;
-  @JsonKey(defaultValue: false)
-  CoverPhoto? coverPhoto;
+  // @JsonKey(defaultValue: false)
+  Map? coverPhoto;
   int followeeCount = 0;
   int followerCount = 0;
   String handle;
   String id;
   bool isVerified = false;
-  @JsonKey(defaultValue: false)
+  // @JsonKey(defaultValue: false)
   String? location;
   String name;
   int playlistCount = 0;
-  @JsonKey(defaultValue: false)
-  ProfilePicture? profilePicture;
+  // @JsonKey(defaultValue: false)
+  Map? profilePicture;
   int repostCount = 0;
   int trackCount = 0;
 
-  // factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
-
-  // Map<String, dynamic> toJson() => _$UserToJson(this);
 }
 
-class CoverPhoto {
-  CoverPhoto({
-    this.p640x,
-    this.p2000x,
-  });
+// class CoverPhoto {
+//   CoverPhoto({
+//     this.p640x,
+//     this.p2000x,
+//   });
 
-  final String? p640x;
-  final String? p2000x;
-}
+//   final String? p640x;
+//   final String? p2000x;
+// }
 
-class ProfilePicture {
-  ProfilePicture({
-    this.p150x150,
-    this.p480x480,
-    this.p1000x1000,
-  });
+// class ProfilePicture {
+//   ProfilePicture({
+//     this.p150x150,
+//     this.p480x480,
+//     this.p1000x1000,
+//   });
 
-  final String? p150x150;
-  final String? p480x480;
-  final String? p1000x1000;
-}
+//   final String? p150x150;
+//   final String? p480x480;
+//   final String? p1000x1000;
+// }
 
 // Do I actually need this????
 class UserResponse {
