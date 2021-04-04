393c5ca1c87fbdcc04425db392de74556fae9eba
Gavin Zhao <74938940+GZGavinZhao@users.noreply.github.com>
2021-04-03 20:55:42 -0400

    Revert "track2MediaItem parsing"
    This reverts commit 66bde60e04fcb4fa6801563c1b84c694b8da49f7.

--- lib/audio/convert2media.dart
+++ lib/audio/convert2media.dart
@@ -5,67 +5,52 @@ import 'package:just_audio/just_audio.dart';
 
 class Parsing {
   static List<Map<String, dynamic>> track2Map(List<Track> tracks) {
-    try {
-      return tracks
-          .map((track) => {
-                'id': track.id,
-                'album': track.title,
-                'title': track.title,
-                'artist': track.user.name,
-                'artUri': track.artwork?['1000x1000'] ??
-                    "https://i1.sndcdn.com/artworks-000666434224-2kg26y-t500x500.jpg",
-                'extras':
-                    'https://dp01.audius.endl.net/v1/tracks/${track.id}/stream?app_name=EXAMPLEAPP'
-              })
-          .toList();
-    } catch (e) {
-      throw Exception(e);
-    }
-  }
-
-  static List<MediaItem> map2MediaItem(List<Map<String, dynamic>> maps) {
-    try {
-      return maps
-          .map((map) => MediaItem(
-                id: map['id'],
-                album: map['album'],
-                title: map['title'],
-                artist: map['artist'],
-                artUri: Uri.parse(map['artUri']),
-                extras: {'stream': Uri.parse(map['extras'])},
-              ))
-          .toList();
-    } catch (e) {
-      throw Exception(e);
-    }
-  }
-
-  static ConcatenatingAudioSource mediaItem2AudioSource(
-      List<MediaItem> mediaItems) {
-    try {
-      return ConcatenatingAudioSource(
-        // children: mediaItems.map((mediaItem) => AudioSource.uri(mediaItem.extras!['stream'])).toList(),
-        children: mediaItems
-            .map((item) => AudioSource.uri(Uri.parse(item.id)))
-            .toList(),
-      );
-    } catch (e) {
-      throw Exception(e);
-    }
-  }
-
-  static List<MediaItem> track2MediaItem(List<Track> tracks) {
+  try {
     return tracks
-        .map((track) => MediaItem(
-            id: track.id,
-            album: track.title,
-            title: track.title,
-            artist: track.user.name,
-            artUri: Uri.parse(track.artwork?['1000x1000'] ??
-                "https://i1.sndcdn.com/artworks-000666434224-2kg26y-t500x500.jpg")))
+        .map((track) => {
+              'id': track.id,
+              'album': track.title,
+              'title': track.title,
+              'artist': track.user.name,
+              'artUri': track.artwork?['1000x1000'] ?? 'none',
+              'extras':
+                  'https://dp01.audius.endl.net/v1/tracks/${track.id}/stream?app_name=EXAMPLEAPP'
+            })
         .toList();
+  } catch (e) {
+    throw Exception(e);
   }
 }
+
+static List<MediaItem> map2MediaItem(List<Map<String, dynamic>> maps) {
+  try {
+    return maps
+        .map((map) => MediaItem(
+              id: map['id'],
+              album: map['album'],
+              title: map['title'],
+              artist: map['artist'],
+              artUri: Uri.parse(map['artUri']),
+              extras: {'stream': Uri.parse(map['extras'])},
+            ))
+        .toList();
+  } catch (e) {
+    throw Exception(e);
+  }
+}
+
+static ConcatenatingAudioSource mediaItem2AudioSource(List<MediaItem> mediaItems) {
+  try {
+    return ConcatenatingAudioSource(
+      // children: mediaItems.map((mediaItem) => AudioSource.uri(mediaItem.extras!['stream'])).toList(),
+      children: mediaItems.map((item) => AudioSource.uri(Uri.parse(item.id))).toList(),
+    );
+  } catch (e) {
+    throw Exception(e);
+  }
+}
+}
+
 
 // class AudioMetadata {
 //   final String album;
